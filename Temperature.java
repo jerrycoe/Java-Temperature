b57c34b7b1c81341ebfbad6b7f31fa733b5c3d20
jerrycoe <jerrycoe2@gmail.com>
2018-04-07 21:07:31 -0700

    complete, need algorithm

--- Temperature.java
+++ Temperature.java
@@ -1,3 +1,51 @@
 class Temperature{
+    private double temperature;
+    private char scale;
 
+    public void setValue(double temperature)
+    {
+        this.temperature = temperature;
+    }
+
+    public void setScale(char scale)
+    {
+        switch(scale)
+        {
+            case 'F': case 'f':
+                this.scale = 'F';
+                break;
+            case 'C': case 'c': default:
+                this.scale = 'C';
+                break;
+        }
+    }
+
+    public void setAll(double temperature, char scale)
+    {
+        setValue(temperature);
+        setScale(scale);
+    }
+
+    public double getCelsius()
+    {
+        if(scale == 'F')
+        {
+            return ((this.temperature - 32) * 5)/9;
+        }
+        return this.temperature;
+
+    }
+
+    public double getFahrenheit(){
+
+        if(scale == 'C'){
+            return 1.8 * this.temperature + 32;
+        }
+        return this.temperature;
+
+    }
+
+    public String toString() {
+        return "Temp is " + temperature + "\u00B0 " + scale;
+    }
 }
